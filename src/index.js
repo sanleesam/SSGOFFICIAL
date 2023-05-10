15574aa57bc4619cb1ff770787c4f386ae435d0e
sanleesam <sanleesam112@gmail.com>
2023-05-10 16:58:22 +0530

    initial commit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+const { Client, IntentsBitField, ActivityType } = require("discord.js");
+
+const client = new Client({
+  intents: [
+    IntentsBitField.Flags.Guilds,
+    IntentsBitField.Flags.GuildMembers,
+    IntentsBitField.Flags.GuildMessages,
+    IntentsBitField.Flags.MessageContent,
+  ],
+});
+
+client.on("ready", (c) => {
+  client.user.setActivity({
+    name: "Sub To Sanlee",
+    type: ActivityType.Listening,
+  });
+});
+
+client.login(process.env.TOKEN);
