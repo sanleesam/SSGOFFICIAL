9d32985d72d41ca4d1c174df04f9e7d8a49500d7
ItzNesbro Dev <asnesbeer3@gmail.com>
2023-05-26 11:59:29 +0000

    updated ping

--- src/SlashCommands/info/ping.js
+++ src/SlashCommands/info/ping.js
@@ -2,6 +2,7 @@ const { Command } = require("reconlx");
 
 module.exports = new Command({
   name: "ping",
+  category: "Info",
   description: "Return The Ping.",
   run: async ({ client, interaction, args }) => {
     interaction.followUp({ content: `${client.ws.ms}Ms!` });
